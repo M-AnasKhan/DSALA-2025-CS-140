103e3347719ad4e88081e6fe67057790d6aac60e
M-AnasKhan <wahmiooooo7@gmail.com>
2026-03-13 16:10:31 +0500

    fix: largest element

--- Lab4/firstProgram.cpp
+++ Lab4/firstProgram.cpp
@@ -2,22 +2,22 @@
 
 #include<iostream>
 #include <iterator>
+
 int main()
 {
     int arr[] = {1,2,3,4,5,6,7,8,9,10};
-
     int count = 0;
-  
-    int lengthOfArray = sizeof(arr) / sizeof(arr[0]);
+    int lengthOfArray = sizeof(arr) / sizeof(arr[0]);  // Important concept 
+    int max = arr[0];
 
     for(int i = 0 ; i < lengthOfArray ; i++)
     {
-
-        std::cout << arr[i] << " ";
-        count++;
-
+        if( arr[i+1] > max)
+        {
+            max = arr[i+1];
+        }
     }
 
-    std::cout << "The the maximum number of element in array are : "<< count ;
+    std::cout << "The maximum values is "<< max;
 
 }
